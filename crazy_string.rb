0ede471a1a335ae87635890513ab810819869981
lauren-hering <lauren.hering@comcast.net>
2018-08-08 01:09:37 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,8 +1,9 @@
 # write your method here
 
-def crazy_strings("a","b")
-a=a.reverse.upcase
-b=b.gsub("s","z").gsub("S","Z").swapcase
+def crazy_strings(a,b)
+return a=a.reverse.upcase
+return b=b.gsub("s", "z").gsub("S", "Z").swapcase
+
 end
 
-crazy_strings("hello","Friends")
+crazy_strings("apple","Snake")
